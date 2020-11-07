c59c2ff9631978fe4268f0d0ab6218c2ad2ee2dd
Jean-Yves BAUDY <baudy.jy@fr.ibm.com>
2020-11-07 23:06:27 +0100

    zUnit Test

--- cics-genapp/base/src/COBOL/LGICDB01.cbl
+++ cics-genapp/base/src/COBOL/LGICDB01.cbl
@@ -18,7 +18,7 @@
       * Select customer details from DB2 table                         *
       *                                                                *
       *                                                                *
-      * MODIF: 11                                                      *
+      * MODIF: 12                                                      *
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. LGICDB01.
